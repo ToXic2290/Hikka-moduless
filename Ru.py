bd4bd60cc03f9171524cb3f1b01a2b894f982d04
ToXic2290 <87765394+ToXic2290@users.noreply.github.com>
2022-05-31 17:52:24 +0600

    Update Ru.py

--- Ru.py
+++ Ru.py
@@ -11,8 +11,62 @@ logger = logging.getLogger(__name__)
 class MorzeMod(loader.Module):
     """Конвертация текста на норм язык"""
 
-    strings = {"name": "NormYazik"}
+    strings = {"name": "Tss..."}
 
+    @loader.unrestricted
+    async def xxcmd(self, message):
+        """.xx [реплай или текст]"""
+        de = {
+            "А": "f ",
+            "Б": ", ",
+            "В": "d ",
+            "Г": "u ",
+            "Д": "l ",
+            "Е": "t ",
+            "Ж": "; ",
+            "З": "p ",
+            "И": "b ",
+            "Й": "q ",
+            "К": "r ",
+            "Л": "k ",
+            "М": "v ",
+            "Н": "y ",
+            "О": "j ",
+            "П": "g ",
+            "Р": "h ",
+            "С": "c ",
+            "Т": "n ",
+            "У": "e ",
+            "Ф": "a ",
+            "Х": "[ ",
+            "Ц": "w ",
+            "Ч": "x ",
+            "Ш": "i ",
+            "Щ": "o ",
+            "Ъ": "] ",
+            "Ы": "s ",
+            "Ь": "m ",
+            "Э": "' ",
+            "Ю": ". ",
+            "Я": "z ",
+        }
+
+        reply = await message.get_reply_message()
+        text = utils.get_args_raw(message)
+
+        if reply and not text:
+            text = reply.raw_text
+        if not text:
+            return await utils.answer(
+                message, "<code>Вы не ввели текст или не сделали реплай.</code>"
+            )
+        x = ""
+        for word in text.split():
+
+            for letter in word.upper():
+                x += de[letter]
+            x += " "
+        await message.edit(x)
 
     @loader.unrestricted
     async def rucmd(self, message):
