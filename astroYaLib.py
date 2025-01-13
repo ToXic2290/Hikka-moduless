964dedfd72504c200fff5619eda5b38f18d4b161
toxicuse <87765394+ToXic2290@users.noreply.github.com>
2025-01-13 19:20:06 +0500

    Add files via upload
    test

---
+++ astroYaLib.py
@@ -0,0 +1,129 @@
+from .. import loader
+
+import random
+import string
+import json
+import aiohttp
+import asyncio
+
+class GameCheat_Lib(loader.Library):
+  developer = "AstroModules"
+  version = (1, 0, 0)
+
+  async def get_current_track(self, client, token):
+  	device_info = {"app_name": "Chrome", "type": 1}
+    ws_proto = {
+        "Ynison-Device-Id": "".join([random.choice(string.ascii_lowercase) for _ in range(16)]),
+        "Ynison-Device-Info": json.dumps(device_info),
+    }
+    timeout = aiohttp.ClientTimeout(total=15, connect=10)
+   	
+   	try:
+        async with aiohttp.ClientSession(timeout=timeout) as session:
+            async with session.ws_connect(
+                url="wss://ynison.music.yandex.ru/redirector.YnisonRedirectService/GetRedirectToYnison",
+                headers={
+                    "Sec-WebSocket-Protocol": f"Bearer, v2, {json.dumps(ws_proto)}",
+                    "Origin": "http://music.yandex.ru",
+                    "Authorization": f"OAuth {token}",
+                },
+                timeout=10,
+            ) as ws:
+                recv = await ws.receive()
+                data = json.loads(recv.data)
+
+            if "redirect_ticket" not in data or "host" not in data:
+                print(f"Invalid response structure: {data}")
+                return {"success": False}
+
+            new_ws_proto = ws_proto.copy()
+            new_ws_proto["Ynison-Redirect-Ticket"] = data["redirect_ticket"]
+
+            to_send = {
+                "update_full_state": {
+                    "player_state": {
+                        "player_queue": {
+                            "current_playable_index": -1,
+                            "entity_id": "",
+                            "entity_type": "VARIOUS",
+                            "playable_list": [],
+                            "options": {"repeat_mode": "NONE"},
+                            "entity_context": "BASED_ON_ENTITY_BY_DEFAULT",
+                            "version": {
+                                "device_id": ws_proto["Ynison-Device-Id"],
+                                "version": 9021243204784341000,
+                                "timestamp_ms": 0,
+                            },
+                            "from_optional": "",
+                        },
+                        "status": {
+                            "duration_ms": 0,
+                            "paused": True,
+                            "playback_speed": 1,
+                            "progress_ms": 0,
+                            "version": {
+                                "device_id": ws_proto["Ynison-Device-Id"],
+                                "version": 8321822175199937000,
+                                "timestamp_ms": 0,
+                            },
+                        },
+                    },
+                    "device": {
+                        "capabilities": {
+                            "can_be_player": True,
+                            "can_be_remote_controller": False,
+                            "volume_granularity": 16,
+                        },
+                        "info": {
+                            "device_id": ws_proto["Ynison-Device-Id"],
+                            "type": "WEB",
+                            "title": "Chrome Browser",
+                            "app_name": "Chrome",
+                        },
+                        "volume_info": {"volume": 0},
+                        "is_shadow": True,
+                    },
+                    "is_currently_active": False,
+                },
+                "rid": "ac281c26-a047-4419-ad00-e4fbfda1cba3",
+                "player_action_timestamp_ms": 0,
+                "activity_interception_type": "DO_NOT_INTERCEPT_BY_DEFAULT",
+            }
+
+            async with session.ws_connect(
+                url=f"wss://{data['host']}/ynison_state.YnisonStateService/PutYnisonState",
+                headers={
+                    "Sec-WebSocket-Protocol": f"Bearer, v2, {json.dumps(new_ws_proto)}",
+                    "Origin": "http://music.yandex.ru",
+                    "Authorization": f"OAuth {token}",
+                },
+                timeout=10,
+                method="GET",
+            ) as ws:
+                await ws.send_str(json.dumps(to_send))
+                recv = await asyncio.wait_for(ws.receive(), timeout=10)
+                ynison = json.loads(recv.data)
+                track_index = ynison["player_state"]["player_queue"]["current_playable_index"]
+                if track_index == -1:
+                    print("No track is currently playing.")
+                    return {"success": False}
+
+                track = ynison["player_state"]["player_queue"]["playable_list"][track_index]
+
+            await session.close()
+            info = await client.tracks_download_info(track["playable_id"], True)
+            track = await client.tracks(track["playable_id"])
+            return {
+                "paused": ynison["player_state"]["status"]["paused"],
+                "duration_ms": ynison["player_state"]["status"]["duration_ms"],
+                "progress_ms": ynison["player_state"]["status"]["progress_ms"],
+                "entity_id": ynison["player_state"]["player_queue"]["entity_id"],
+                "repeat_mode": ynison["player_state"]["player_queue"]["options"]["repeat_mode"],
+                "entity_type": ynison["player_state"]["player_queue"]["entity_type"],
+                "track": track,
+                "info": info,
+                "success": True,
+            }
+
+    except Exception as e:
+        return {"success": False, "error": str(e), "track": None}
